55762ebcd7042a868773fc10ee847a8c4419880c
Tae Hyun Lee <t_lee51@u.pacific.edu>
2020-06-08 21:55:48 -0700

    final

--- Alcohol Calculator/src/EnterDrinkScreen.java
+++ Alcohol Calculator/src/EnterDrinkScreen.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.MouseEvent;
-
+import java.text.DecimalFormat;
 import java.awt.TextField;
 
 import acm.graphics.GImage;
@@ -23,22 +23,18 @@ public class EnterDrinkScreen extends GraphicsPane{
 	private GRect beerRect;
 	private GRect shotRect;
 	private GRect wineRect;
-	private GRect customRect;
 	
 	private GRect beerLabelbox;
 	private GRect shotLabelbox;
 	private GRect wineLabelbox;
-	private GRect customLabelbox;
 	
 	private GLabel beerLabel;
 	private GLabel shotLabel;
 	private GLabel wineLabel;
-	private GLabel customLabel;
 	
 	private GLabel beerCount;
 	private GLabel shotCount;
 	private GLabel wineCount;
-	private GLabel customCount;
 	
 	private GImage beerAdd;
 	private GImage beerSub;
@@ -46,8 +42,6 @@ public class EnterDrinkScreen extends GraphicsPane{
 	private GImage wineSub;
 	private GImage shotAdd;
 	private GImage shotSub;
-	private GImage customAdd;
-	private GImage customSub;
 	
 	private GLabel drinkingTimeLabel;
 	private GLabel drinkingTime;
@@ -75,6 +69,11 @@ public class EnterDrinkScreen extends GraphicsPane{
 	
 	private TextField weightInput;
 	
+	private double a;
+	private int w;
+	private double r;
+	private double h;
+	
 	public EnterDrinkScreen(MainApplication app) {
 		super();
 		program = app;
@@ -84,98 +83,82 @@ public class EnterDrinkScreen extends GraphicsPane{
 		title.setFont(new Font("Arial", Font.BOLD, 35));
 		title.setColor(new Color(255,255,255));
 		
-		beer = new GImage("beer.png", 80,75);
-		wine = new GImage("wine.png", 290, 80);
-		shot = new GImage("shot.png", 480, 75);
+		beer = new GImage("beer.png", 110,75);
+		wine = new GImage("wine.png", 380, 80);
+		shot = new GImage("shot.png", 610, 75);
 		
 		beer.setSize(50,100);
 		wine.setSize(50,90);
 		shot.setSize(100,100);
 		
-		beerRect = new GRect(10,60,200,130);
+		beerRect = new GRect(10,60,250,130);
 		beerRect.setFillColor(new Color(230,249,255));
 		beerRect.setColor(new Color(230,249,255));
 		beerRect.setFilled(true);
 		
 
-		wineRect = new GRect(220,60,200,130);
+		wineRect = new GRect(270,60,250,130);
 		wineRect.setFillColor(new Color(230,249,255));
 		wineRect.setColor(new Color(230,249,255));
 		wineRect.setFilled(true);
 		
-		shotRect = new GRect(430,60,200,130);
+		shotRect = new GRect(530,60,250,130);
 		shotRect.setFillColor(new Color(230,249,255));
 		shotRect.setColor(new Color(230,249,255));
 		shotRect.setFilled(true);
 		
-		customRect = new GRect(640,60,150,130);
-		customRect.setFillColor(new Color(230,249,255));
-		customRect.setColor(new Color(230,249,255));
-		customRect.setFilled(true);
 		
-		beerLabelbox = new GRect(10,200,200,100);
+		beerLabelbox = new GRect(10,200,250,100);
 		beerLabelbox.setFillColor(new Color(230,249,255));
 		beerLabelbox.setColor(new Color(230,249,255));
 		beerLabelbox.setFilled(true);
 		
-		wineLabelbox = new GRect(220, 200 ,200,100);
+		wineLabelbox = new GRect(270, 200 ,250,100);
 		wineLabelbox.setFillColor(new Color(230,249,255));
 		wineLabelbox.setColor(new Color(230,249,255));
 		wineLabelbox.setFilled(true);
 
-		shotLabelbox = new GRect(430,200,200,100);
+		shotLabelbox = new GRect(530,200,250,100);
 		shotLabelbox.setFillColor(new Color(230,249,255));
 		shotLabelbox.setColor(new Color(230,249,255));
 		shotLabelbox.setFilled(true);
 		
-		customLabelbox = new GRect(640,200,150,100);
-		customLabelbox.setFillColor(new Color(230,249,255));
-		customLabelbox.setColor(new Color(230,249,255));
-		customLabelbox.setFilled(true);
 		
-		beerLabel = new GLabel("BEER", 80, 230);
+		beerLabel = new GLabel("12oz  4.5%  BEER", 30, 230);
 		beerLabel.setFont(new Font("Arial", Font.BOLD, 20));
 		beerLabel.setColor(new Color(0,0,0));
 
-		wineLabel = new GLabel("WINE", 290, 230);
+		wineLabel = new GLabel("6oz  12%   WINE", 290, 230);
 		wineLabel.setFont(new Font("Arial", Font.BOLD, 20));
 		wineLabel.setColor(new Color(0,0,0));
 		
-		shotLabel = new GLabel("SHOT", 500, 230);
+		shotLabel = new GLabel("1oz  40%   SHOT", 550, 230);
 		shotLabel.setFont(new Font("Arial", Font.BOLD, 20));
 		shotLabel.setColor(new Color(0,0,0));
 		
-		customLabel = new GLabel("CUSTOM", 660, 230);
-		customLabel.setFont(new Font("Arial", Font.BOLD, 20));
-		customLabel.setColor(new Color(0,0,0));
 		
-		beerCount = new GLabel("0", 90, 280);
+		beerCount = new GLabel("0", 140, 280);
 		beerCount.setFont(new Font("Arial", Font.BOLD, 40));
 		beerCount.setColor(new Color(0,0,0));
 		
-		wineCount = new GLabel("0", 300, 280);
+		wineCount = new GLabel("0", 400, 280);
 		wineCount.setFont(new Font("Arial", Font.BOLD, 40));
 		wineCount.setColor(new Color(0,0,0));
 		
-		shotCount = new GLabel("0", 510, 280);
+		shotCount = new GLabel("0", 660, 280);
 		shotCount.setFont(new Font("Arial", Font.BOLD, 40));
 		shotCount.setColor(new Color(0,0,0));
 		
-		customCount = new GLabel("0", 680, 280);
-		customCount.setFont(new Font("Arial", Font.BOLD, 40));
-		customCount.setColor(new Color(0,0,0));
 		
-		beerAdd = new GImage("arrowUP.png", 160 , 200);
-		beerSub = new GImage("arrowDOWN.png", 160, 250);
+		beerAdd = new GImage("arrowUP.png", 210 , 200);
+		beerSub = new GImage("arrowDOWN.png", 210, 250);
 		
-		wineAdd = new GImage("arrowUP.png", 370 , 200);
-		wineSub = new GImage("arrowDOWN.png", 370, 250);
+		wineAdd = new GImage("arrowUP.png", 470 , 200);
+		wineSub = new GImage("arrowDOWN.png", 470, 250);
 
-		shotAdd = new GImage("arrowUP.png", 580 , 200);
-		shotSub = new GImage("arrowDOWN.png", 580, 250);
+		shotAdd = new GImage("arrowUP.png", 730 , 200);
+		shotSub = new GImage("arrowDOWN.png", 730, 250);
 		
-		customAdd = new GImage("arrowUP.png", 750 , 200);
-		customSub = new GImage("arrowDOWN.png", 750, 250);
 		
 		drinkingTimeLabel = new GLabel("DRINKING TIME :", 160, 350);
 		drinkingTimeLabel.setFont(new Font("Arial", Font.BOLD, 35));
@@ -217,7 +200,15 @@ public class EnterDrinkScreen extends GraphicsPane{
 		settings.setSize(120,50);
 		
 		settingsScreen = new SettingsScreen(app);
-
+		
+		
+		totalCalories = new GLabel("0 kcal", 600, 540);
+		bac = new GLabel("0.00", 600, 420);
+		breakdown = new GLabel("0HR 00MIN", 600, 480);
+		
+		totalCalories.setFont(new Font("Arial", Font.BOLD, 35));
+		bac.setFont(new Font("Arial", Font.BOLD, 35));
+		breakdown.setFont(new Font("Arial", Font.BOLD, 35));
 	}
 
 	@Override
@@ -228,30 +219,24 @@ public class EnterDrinkScreen extends GraphicsPane{
 		program.add(beerRect);
 		program.add(wineRect);
 		program.add(shotRect);
-		program.add(customRect);
 		program.add(beer);
 		program.add(wine);
 		program.add(shot);
 		program.add(beerLabelbox);
 		program.add(wineLabelbox);
 		program.add(shotLabelbox);
-		program.add(customLabelbox);
 		program.add(beerLabel);
 		program.add(wineLabel);
 		program.add(shotLabel);
-		program.add(customLabel);
 		program.add(beerCount);
 		program.add(wineCount);
 		program.add(shotCount);
-		program.add(customCount);
 		program.add(beerAdd);
 		program.add(beerSub);
 		program.add(wineAdd);
 		program.add(wineSub);
 		program.add(shotAdd);
 		program.add(shotSub);
-		program.add(customAdd);
-		program.add(customSub);
 		program.add(drinkingTimeLabel);
 		program.add(drinkingTime);
 		program.add(timeAdd);
@@ -266,6 +251,9 @@ public class EnterDrinkScreen extends GraphicsPane{
 		program.add(bacLabel);
 		program.add(breakdownLabel);
 		program.add(settings);
+		program.add(totalCalories);
+		program.add(bac);
+		program.add(breakdown);
 		
 	}
 
@@ -280,25 +268,19 @@ public class EnterDrinkScreen extends GraphicsPane{
 		program.remove(beerRect);
 		program.remove(wineRect);
 		program.remove(shotRect);
-		program.remove(customRect);
 		program.remove(beerLabelbox);
 		program.remove(wineLabelbox);
 		program.remove(shotLabelbox);
-		program.remove(customLabelbox);
 		program.remove(beerLabel);
 		program.remove(wineLabel);
 		program.remove(shotLabel);
-		program.remove(customLabel);
 		program.remove(beerCount);
 		program.remove(wineCount);
 		program.remove(shotCount);
-		program.remove(customCount);
 		program.remove(beerAdd);
 		program.remove(beerSub);
 		program.remove(wineAdd);
 		program.remove(wineSub);
-		program.remove(customAdd);
-		program.remove(customSub);
 		program.remove(drinkingTimeLabel);
 		program.remove(drinkingTime);
 		program.remove(timeAdd);
@@ -311,9 +293,14 @@ public class EnterDrinkScreen extends GraphicsPane{
 		program.remove(bacBox);
 		program.remove(breakdownBox);
 		program.remove(settings);
+		program.remove(totalCalories);
+		program.remove(bac);
+		program.remove(breakdown);
 	}
 
 	private GObject obj;
+	
+	
 	
 	
 	@Override
@@ -345,14 +332,6 @@ public class EnterDrinkScreen extends GraphicsPane{
 				shotCount.setLabel(Integer.toString(Integer.parseInt(shotCount.getLabel()) - 1));
 			}
 		}
-		else if (obj == customAdd) {
-			customCount.setLabel(Integer.toString(Integer.parseInt(customCount.getLabel()) + 1));
-		}
-		else if (obj == customSub) {
-			if (Integer.parseInt(customCount.getLabel()) > 0) {
-				customCount.setLabel(Integer.toString(Integer.parseInt(customCount.getLabel()) - 1));
-			}
-		}
 		
 		else if (obj == timeAdd) {
 			drinkingTime.setLabel(Double.toString(Double.parseDouble(drinkingTime.getLabel()) + 0.5));
@@ -367,6 +346,34 @@ public class EnterDrinkScreen extends GraphicsPane{
 			program.switchToScreen(settingsScreen);
 		}
 		
+		
+		a = Integer.parseInt(beerCount.getLabel())*12*0.05 + Integer.parseInt(wineCount.getLabel())*6*0.12 + Integer.parseInt(shotCount.getLabel())*1*0.4;
+		w = MainApplication.user.getWeight();
+		if(MainApplication.user.getSex() == "male") {
+			r = 0.73;
+		}
+		else {
+			r = 0.66;
+		}
+		h = Double.parseDouble(drinkingTime.getLabel());
+		
+		
+		if ((a*5.14)/(w*r) - (0.015*h) < 0) {
+			bac.setLabel(Double.toString(0.00));
+		}
+		else {
+			bac.setLabel(String.format("%.2f", ((a*5.14)/(w*r) - (0.015*h)) ) );
+		}
+		
+		
+		double decPart = Double.parseDouble(bac.getLabel())/0.015 - (int) (Double.parseDouble(bac.getLabel())/0.015) ;
+		
+		
+		breakdown.setLabel(Integer.toString((int) (Double.parseDouble(bac.getLabel())/0.015)) + "HR" +  (int) (decPart*60) +"MIN" );
+		
+		
+		int total = Integer.parseInt(beerCount.getLabel())*150 + Integer.parseInt(wineCount.getLabel())*140 + Integer.parseInt(shotCount.getLabel())*100;
+		totalCalories.setLabel(Integer.toString(total) + " kcal");
 		
 	}
 	
--- Alcohol Calculator/src/SettingsScreen.java
+++ Alcohol Calculator/src/SettingsScreen.java
@@ -10,6 +10,9 @@ public class SettingsScreen extends GraphicsPane {
 	private GImage edit;
 	private GImage list;
 	
+	private GImage list1;
+	private GImage list2;
+	
 	public SettingsScreen(MainApplication app) {
 		super();
 		program = app;
@@ -17,12 +20,22 @@ public class SettingsScreen extends GraphicsPane {
 		bg = new GImage("bg2.PNG", 0, 0);
 		edit = new GImage("edit0.png", 300,200);
 		list = new GImage("list0.png", 320, 400);
+		
+		list1 = new GImage("alcohol1.png",0,0);
+		list1.setSize(800, 600);
+		list1.setVisible(false);
+		
+		list2 = new GImage("alcohol2.png", 0,0);
+		list2.setSize(800,600);
+		list2.setVisible(false);
 	}
 
 	@Override
 	public void showContents() 
 	{
 		program.add(bg);
+		program.add(list1);
+		program.add(list2);
 		program.add(edit);
 		program.add(list);
 	}
@@ -33,6 +46,8 @@ public class SettingsScreen extends GraphicsPane {
 		program.remove(bg);
 		program.remove(edit);
 		program.remove(list);
+		program.remove(list1);
+		program.remove(list2);
 	}
 
 	private GObject obj;
@@ -44,6 +59,21 @@ public class SettingsScreen extends GraphicsPane {
 		
 		if (obj == edit) {
 			program.switchToEnterInfo();
+		}
+		else if (obj == list) {
+			list1.sendToFront();
+			list1.setVisible(true);
+			
+		}
+		else if (list1.isVisible() == true && obj == list1) {
+			list1.sendToBack();
+			list1.setVisible(false);
+			list2.sendToFront();
+			list2.setVisible(true);
+		}
+		else if (list1.isVisible() == false && list2.isVisible() == true && obj == list2) {
+			list2.sendToBack();
+			list2.setVisible(false);
 		}
 	}
 	
